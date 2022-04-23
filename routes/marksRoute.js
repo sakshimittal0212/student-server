6b8396ef356f0d5688119b567035389c37e1f77b
unicorn <unicorn@gmail.com>
2022-04-23 19:59:30 +0530

    bug fix

--- routes/marksRoute.js
+++ routes/marksRoute.js
@@ -5,9 +5,9 @@ function attachApi(app){
 //  that we are getting from app.js file 
  // when this function is called
  
- app.get('/api/promise/avgMarks/:studentId',avgMarksPromiseController)
+ app.get('/api/promise/avgMarks/:studentId',marksController.avgMarksPromiseController)
 
- app.get('/api/async/avgMarks/:studentId',avgMarksAsyncController)
+ app.get('/api/async/avgMarks/:studentId',marksController.avgMarksAsyncController)
 }
 
 module.exports=attachApi;
