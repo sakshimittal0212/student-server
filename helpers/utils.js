0ac5690ce9fb1aced359b2fb951f0ee3165f25df
unicorn <unicorn@gmail.com>
2022-04-24 20:13:45 +0530

    otp done

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,6 +1,10 @@
 let generateOtp=function(num){
-    return "12345"
-}
+    var digits = '0123456789';
+    let OTP = '';
+    for (let i = 0; i < num; i++ ) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    return OTP;}
 module.exports={
     generateOtp:generateOtp
 }
